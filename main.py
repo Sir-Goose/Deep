23859d84dfc8907232413c439f4d40e3d23bd7a1
Matthew <66563291+Sir-Goose@users.noreply.github.com>
2022-07-24 21:08:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+text = input("What is the Answer to the Great Question of Live, the Universe, and Everything? ")
+
+if (text == "42"):
+    print("Yes")
+
+elif (text == "Forty Two"):
+    print("Yes")
+
+elif (text == "forty two"):
+    print("Yes")
+
+elif (text == "FoRty TwO"):
+    print("Yes")
+
+elif (text == " 42 "):
+    print("Yes")
+
+
+elif (text == "forty-two"):
+    print("Yes")
+
+else:
+    print("No")
+
+
+
